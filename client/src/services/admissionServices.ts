af9196d505d00c115773cf4df751dbfa21f12f12
sayroll <83714631+sayroll@users.noreply.github.com>
2024-12-05 17:01:57 -0600

    front end change

--- client/src/services/admissionServices.ts
+++ client/src/services/admissionServices.ts
@@ -9,7 +9,7 @@ export interface AdmissionData{
     languageProficiencyType: string,
     languageScore: number,
     GRE: number,
-    notes: string,
+    Notes: string,
     userID: number
 } 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3007";
